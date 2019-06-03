26056d71147b986f43e94c7bba6606eaa36a854f
marlonx19 <marlon.englemam@gmail.com>
2019-06-03 00:30:40 -0300

    Fix

--- src/Components/Config/Config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseAPI = {
-    apiKey: "AIzaSyA3CLSTEZiZPcVglsmRIJwbeZxlu7AvG3w",
-    authDomain: "picshare-da1dc.firebaseapp.com",
-    databaseURL: "https://picshare-da1dc.firebaseio.com",
-    projectId: "picshare-da1dc",
-    storageBucket: "picshare-da1dc.appspot.com",
-    messagingSenderId: "814433119730"
-}
