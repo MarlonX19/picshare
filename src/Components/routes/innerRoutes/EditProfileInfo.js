dd436693c41d9b6ea84d9e9d551659b60806ec30
MarlonX19 <marlon.englemam@gmail.com>
2019-07-04 00:37:21 -0300

    Fix

--- src/Components/routes/innerRoutes/EditProfileInfo.js
+++ src/Components/routes/innerRoutes/EditProfileInfo.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Button, Image, TouchableOpacity, Dimensions } from 'react-native';
-import { TextInput } from 'react-native-gesture-handler';
+import { View, Text, StyleSheet, Button, Image, TouchableOpacity, Dimensions, TextInput } from 'react-native';
+
 
 const HEIGHT = Dimensions.get('window').height;
 const WIDTH = Dimensions.get('window').width;
@@ -17,7 +17,7 @@ class EditProfileInfo extends Component {
                 <View style={styles.headerView}>
                     <Image style={{ width: 100, height: 100, borderRadius: 50, borderWidth: 1, borderColor: 'green' }} source={require('../../imgs/marlonfoto.png')} />
                     <TouchableOpacity
-                        onPress={() => { }}
+                        onPress={() => false}
 
                     >
                         <Text style={{ fontWeight: '700', color: 'blue', fontSize: 18 }}>Mudar foto</Text>
@@ -34,21 +34,18 @@ class EditProfileInfo extends Component {
                     <TextInput
                         style={{ flex: 1, margin: 10, padding: 5, borderBottomWidth: 0.4, borderBottomColor: 'red' }}
                         placeholder='Nome civíl'
-                        label='Nome civíl'
                         onChangeText={() => false}
                         maxLength={50}
                     />
                     <TextInput
                         style={{ flex: 1, margin: 10, padding: 5, borderBottomWidth: 0.4, borderBottomColor: 'red' }}
                         placeholder='Website'
-                        label='Website'
                         onChangeText={() => false}
                         maxLength={50}
                     />
                     <TextInput
                         style={{ flex: 1, margin: 10, padding: 5, borderBottomWidth: 0.4, borderBottomColor: 'red' }}
                         placeholder='Bio'
-                        label='Bio'
                         onChangeText={() => false}
                         maxLength={50}
                     />
